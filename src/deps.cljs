b713a96f0d6108b9f1565a89e2a50d97fe9ffc3a
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-06-29 23:02:30 -0500

    highlight.js v11

--- src/deps.cljs
+++ src/deps.cljs
@@ -3,7 +3,7 @@
   ; shadow-cljs creates package.json 
   ; based on this dependencies 
 
-  "highlight.js" "^10.0.3" ; code-coloring ; also used by 10x
+  "highlight.js" "^11.0.1" ; code-coloring ; also used by 10x
   ;"highlightjs-line-numbers.js" "^2.8.0"
   ; see: https://github.com/wcoder/highlightjs-line-numbers.js/issues/77s
 
